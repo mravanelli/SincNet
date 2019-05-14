86d8042a09d82644f2b2ed67e23b806ae18669bd
Mirco Ravanelli <mirco.ravanelli@gmail.com>
2019-05-14 08:20:38 -0400

    Update TIMIT_preparation.py
    lower/upper case TIMIT file names

--- TIMIT_preparation.py
+++ TIMIT_preparation.py
@@ -13,6 +13,9 @@
 # How to run it:
 # python TIMIT_preparation.py $TIMIT_FOLDER $OUTPUT_FOLDER data_lists/TIMIT_all.scp 
 
+# NOTE: This script expects filenames in lowercase (e.g, train/dr1/fcjf0/si1027.wav" rather than "TRAIN/DR1/FCJF0/SI1027.WAV)
+
+
 import shutil
 import os
 import soundfile as sf
